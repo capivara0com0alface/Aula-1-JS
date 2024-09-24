eb7d7221ced7ae4882d649f4062a774e586ebc90
Marina <137818327+capivara0com0alface@users.noreply.github.com>
2024-09-24 11:53:46 -0300

    c

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function areaQuadrado(number3,number4){
 return number3*number4;
 
 }
-console.log(areaQuarado(4,5));
+
 
 let areatrapezio=function(number6,number7,number8){
 return number7+number6*number8/2;
